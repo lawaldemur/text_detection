c730b0c0b97431c3278c1f25dfdf83f22347f374
lawaldemor <vkartam@gmail.com>
2020-12-11 13:52:42 +0300

    update structure

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ from detect import detect
 
 # local: for mac os 0.0.0.0:5001, for windows 127.0.0.1:30
 ip, port = '0.0.0.0', 5001
-app = Flask(__name__, static_url_path='', static_folder=os.getcwd() + '/images/')
+app = Flask(__name__, static_url_path='', static_folder=os.getcwd() + '/static/')
 
 
 @app.route('/')
 def index():
     # Video streaming home page.
-    return render_template('index.html')
+    return render_template('/templates/index.html')
 
 
 @app.route('/process', methods=['POST'])
 def process_img():
     file = request.files['image']
-    path = os.path.join(os.getcwd() + '/images/', file.filename)
+    path = os.path.join(os.getcwd() + '/static/uploads/', file.filename)
     file.save(path)
 
     return detect(path)
